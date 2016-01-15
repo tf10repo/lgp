017caf13395c78641f9db32c150153ae7e45686e
NeonElectron <gapashu@gmail.com>
2016-01-14 20:22:58 -0500

    things

---
+++ lua/autorun/sh_load.lua
@@ -0,0 +1,8 @@
+LGP = LGP or {}
+LGP.__index = LGP
+
+if SERVER then
+AddCSLuaFile("lgp/cl_lgpeditor.lua")
+AddCSLuaFile("lgp/cl_lgphooks.lua")
+end
+
---
+++ lua/lgp/cl_lgphooks.lua
@@ -0,0 +1,10 @@
+
+
+function LGP.Render()
+	local Entities = ents.FindByClass("gmod_wire_lgp")
+	for Index, Entity in pairs(Entities) do
+		Entity:Render()
+	end
+end
+
+hook.Add("PostDrawTranslucentRenderables", "LGPHudPaint", LGP.Render)
