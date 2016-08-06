22847ab3d5689ee2b395f6ddf5bf9026335fec63
Starkkz <matiasahermosilla@gmail.com>
2016-08-06 17:59:21 -0300

    Fixed autorun typo

--- lua/autorun/sh_lgpload.lua
+++ lua/autorun/sh_lgpload.lua
@@ -17,17 +17,17 @@ if CLIENT then
 	end
 elseif SERVER then
 	-- Run all the sv_ files
-	for _, Filename in SortedPairs(file.Find("peelpad/sv_*.lua", "LUA"), true) do
+	for _, Filename in SortedPairs(file.Find("lgp/sv_*.lua", "LUA"), true) do
 		include("lgp/"..Filename)
 	end
 
 	-- Transfer all the cl_ files to the client
-	for _, Filename in SortedPairs(file.Find("peelpad/cl_*.lua", "LUA"), true) do
+	for _, Filename in SortedPairs(file.Find("lgp/cl_*.lua", "LUA"), true) do
 		AddCSLuaFile("lgp/"..Filename)
 	end
 
 	-- Transfer all the sh_ files to the client, and run them
-	for _, Filename in SortedPairs(file.Find("peelpad/sh_*.lua", "LUA"), true) do
+	for _, Filename in SortedPairs(file.Find("lgp/sh_*.lua", "LUA"), true) do
 		AddCSLuaFile("lgp"..Filename)
 		include("lgp/"..Filename)
 	end
